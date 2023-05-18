c8f7471ff5a89a60b29608fa65ee64bc4d1f05e9
jkuo630 <89288700+jkuo630@users.noreply.github.com>
2023-05-18 18:03:23 -0400

    finished about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,22 @@
-import { Divider, Stack, Text, Container, Box, HStack } from "@chakra-ui/react";
+import {
+  Icon,
+  Divider,
+  Stack,
+  Text,
+  Container,
+  Box,
+  HStack,
+} from "@chakra-ui/react";
+import { DiJsBadge } from "react-icons/di";
+import { DiCss3 } from "react-icons/di";
+import { DiHtml5 } from "react-icons/di";
+import { DiJavascript } from "react-icons/di";
+import { DiMongodb } from "react-icons/di";
+import { DiNodejs } from "react-icons/di";
+import { DiPython } from "react-icons/di";
+import { DiReact } from "react-icons/di";
+import { DiWordpress } from "react-icons/di";
+
 export default function About({ color }) {
   return (
     <>
@@ -26,8 +44,19 @@ export default function About({ color }) {
             through my professional and extracurricular endeavours. When I'm not
             coding, you can find me hitting the gym, playing the newest video
             games, polishing my stock portfolio or catching up with my favourite
-            animes! Here is my tech stack:
+            animes! Here are just some of the tech I've worked with!
           </Text>
+          <HStack spacing="50px">
+            <Icon as={DiJsBadge} w={10} h={10} color="yellow.500" />
+            <Icon as={DiCss3} w={10} h={10} color="blue.500" />
+            <Icon as={DiHtml5} w={10} h={10} color="orange.500" />
+            <Icon as={DiJavascript} w={10} h={10} color="yellow.500" />
+            <Icon as={DiMongodb} w={10} h={10} color="green.500" />
+            <Icon as={DiNodejs} w={10} h={10} />
+            <Icon as={DiPython} w={10} h={10} />
+            <Icon as={DiReact} w={10} h={10} color="blue.500" />
+            <Icon as={DiWordpress} w={10} h={10} color="blue.500" />
+          </HStack>
         </Stack>
       </Container>
     </>
